563a9e73bbbdbfd9f400fa36af989e0279f44ca7
jgyb89 <jgreen.gus@gmail.com>
2025-10-19 18:12:20 -0700

    Coded looping through an array of objects

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,32 @@
+// Card Array
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
+
+// Form Variables
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileBtn = document.querySelector(".profile__edit-profile_button");
 const editProfileExit = editProfileModal.querySelector(".modal__exit");
@@ -16,6 +45,7 @@ const newPostLinkInput = newPostModal.querySelector("#link-input");
 const newPostCaptionInput = newPostModal.querySelector("#caption-input");
 const newPostForm = newPostModal.querySelector("#new-post-form");
 
+// Open Modal Function
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
 }
@@ -58,3 +88,11 @@ function handleNewPostFormSubmit(evt) {
   closeModal(newPostModal);
 }
 newPostForm.addEventListener("submit", handleNewPostFormSubmit);
+
+const cardName = initialCards.map(function (item) {
+  return item.name;
+});
+
+cardName.forEach(function (name) {
+  console.log(name);
+});
